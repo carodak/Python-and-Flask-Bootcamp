02c0b57ce7eb6f301652b32b7285991bd58cf2fc
crispstudies <carodak2@gmail.com>
2022-07-11 00:01:55 -0400

    Finished decorators lesson

--- Personal Work/Section 6 - Python - Level Two/57-decorators.py
+++ Personal Work/Section 6 - Python - Level Two/57-decorators.py
@@ -4,7 +4,41 @@ def hello(name='Jose'):
         return "This is inside the greet()"
     def welcome():
         return "This is inside welcome()"
-    print(greet())
-    print(welcome())
 
-hello()
+    if name == "Jose":
+        return greet
+    else:
+        return welcome
+# x = greet
+x = hello()
+print(x()) #since we did not return greet(), we can return x()
+
+x = hello(name="Sammy")
+print(x())
+
+def hello():
+    return "Hi Jose"
+
+def other(func):
+    print("Some other code")
+    print(func()) #we are calling the function
+
+other(hello) #we are just passing the function not calling it
+
+def new_decorator(func):
+
+    def wrap_func():
+        print("Some code before executing func")
+
+        func()
+
+        print("Code here, after executing func()")
+
+    return wrap_func
+
+@new_decorator
+def func_needs_decorator():
+    print("Please decorate me!!")
+
+#func_needs_decorator = new_decorator(func_needs_decorator)
+func_needs_decorator()
